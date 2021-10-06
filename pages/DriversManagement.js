7c0546cf84f93d0117e506b89d80e291a6265593
desgbr <des.gbr@gmail.com>
2021-10-06 09:28:56 +0200

    handling

--- pages/DriversManagement.js
+++ pages/DriversManagement.js
@@ -1,133 +1,60 @@
 import React from "react";
-import { Row, Col,Navbar,Container,Nav,NavDropdown,Dropdown, Button } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Dropdown,
+  Button,
+} from "react-bootstrap";
 import Card from "../components/Card";
 
 const Data_table = [
   {
-    'check':'check',
-    'name': 'Demo Moaz',
-    'plateNumber': '358480085000471',
-    'mainType': 'Vechile Wash',
-    'periodTyoe':'By Fixed Date',
-    'startValue':'09/26/2021',
-    'nextValue':'09/19/2021',
-    'recurring':'Exist',
-    'notifyPeriod':'Value',
-    'notifyWhenValue':'0',
-    'overdue':'yes',
-    'action':'reset/edit/delete',
+    name: "Demo Moaz",
+    phoneNumber: "0539448802",
+    email: "email@saferoad.com",
+    department: "By Fixed Date",
+    licenceNumber: "142666",
+    licenceEXP: "09/19/2021",
+    rfid: "Exist",
+    waslIntgeration: "yes",
+    action: "edit/del/locate driver/show/assigned vehicle",
   },
   {
-    'check':'check',
-    'name': 'Demo Moaz',
-    'plateNumber': '358480085000471',
-    'mainType': 'Vechile Wash',
-    'periodTyoe':'By Fixed Date',
-    'startValue':'09/26/2021',
-    'nextValue':'09/19/2021',
-    'recurring':'Exist',
-    'notifyPeriod':'Value',
-    'notifyWhenValue':'0',
-    'overdue':'yes',
-    'action':'reset/edit/delete',
+    name: "Demo Moaz",
+    phoneNumber: "0539448802",
+    email: "email@saferoad.com",
+    department: "By Fixed Date",
+    licenceNumber: "142666",
+    licenceEXP: "09/19/2021",
+    rfid: "Exist",
+    waslIntgeration: "yes",
+    action: "edit/del/locate driver/show/assigned vehicle",
   },
   {
-    'check':'check',
-    'name': 'Demo Moaz',
-    'plateNumber': '358480085000471',
-    'mainType': 'Vechile Wash',
-    'periodTyoe':'By Fixed Date',
-    'startValue':'09/26/2021',
-    'nextValue':'09/19/2021',
-    'recurring':'Exist',
-    'notifyPeriod':'Value',
-    'notifyWhenValue':'0',
-    'overdue':'yes',
-    'action':'reset/edit/delete',
+    name: "Demo Moaz",
+    phoneNumber: "0539448802",
+    email: "email@saferoad.com",
+    department: "By Fixed Date",
+    licenceNumber: "142666",
+    licenceEXP: "09/19/2021",
+    rfid: "Exist",
+    waslIntgeration: "yes",
+    action: "edit/del/locate driver/show/assigned vehicle",
   },
   {
-    'check':'check',
-    'name': 'Demo Moaz',
-    'plateNumber': '358480085000471',
-    'mainType': 'Vechile Wash',
-    'periodTyoe':'By Fixed Date',
-    'startValue':'09/26/2021',
-    'nextValue':'09/19/2021',
-    'recurring':'Exist',
-    'notifyPeriod':'Value',
-    'notifyWhenValue':'0',
-    'overdue':'yes',
-    'action':'reset/edit/delete',
-  },
-  {
-    'check':'check',
-    'name': 'Demo Moaz',
-    'plateNumber': '358480085000471',
-    'mainType': 'Vechile Wash',
-    'periodTyoe':'By Fixed Date',
-    'startValue':'09/26/2021',
-    'nextValue':'09/19/2021',
-    'recurring':'Exist',
-    'notifyPeriod':'Value',
-    'notifyWhenValue':'0',
-    'overdue':'yes',
-    'action':'reset/edit/delete',
-  },
-  {
-    'check':'check',
-    'name': 'Demo Moaz',
-    'plateNumber': '358480085000471',
-    'mainType': 'Vechile Wash',
-    'periodTyoe':'By Fixed Date',
-    'startValue':'09/26/2021',
-    'nextValue':'09/19/2021',
-    'recurring':'Exist',
-    'notifyPeriod':'Value',
-    'notifyWhenValue':'0',
-    'overdue':'yes',
-    'action':'reset/edit/delete',
-  },
-  {
-    'check':'check',
-    'name': 'Demo Moaz',
-    'plateNumber': '358480085000471',
-    'mainType': 'Vechile Wash',
-    'periodTyoe':'By Fixed Date',
-    'startValue':'09/26/2021',
-    'nextValue':'09/19/2021',
-    'recurring':'Exist',
-    'notifyPeriod':'Value',
-    'notifyWhenValue':'0',
-    'overdue':'yes',
-    'action':'reset/edit/delete',
-  },
-  {
-    'check':'check',
-    'name': 'Demo Moaz',
-    'plateNumber': '358480085000471',
-    'mainType': 'Vechile Wash',
-    'periodTyoe':'By Fixed Date',
-    'startValue':'09/26/2021',
-    'nextValue':'09/19/2021',
-    'recurring':'Exist',
-    'notifyPeriod':'Value',
-    'notifyWhenValue':'0',
-    'overdue':'yes',
-    'action':'reset/edit/delete',
-  },
-  {
-    'check':'check',
-    'name': 'Demo Moaz',
-    'plateNumber': '358480085000471',
-    'mainType': 'Vechile Wash',
-    'periodTyoe':'By Fixed Date',
-    'startValue':'09/26/2021',
-    'nextValue':'09/19/2021',
-    'recurring':'Exist',
-    'notifyPeriod':'Value',
-    'notifyWhenValue':'0',
-    'overdue':'yes',
-    'action':'reset/edit/delete',
+    name: "Demo Moaz",
+    phoneNumber: "0539448802",
+    email: "email@saferoad.com",
+    department: "By Fixed Date",
+    licenceNumber: "142666",
+    licenceEXP: "09/19/2021",
+    rfid: "Exist",
+    waslIntgeration: "yes",
+    action: "edit/del/locate driver/show/assigned vehicle",
   },
 ];
 
@@ -138,34 +65,18 @@ export default function PreventiveMaintenance() {
       <Row>
         <Col sm="12">
           <Card>
+            <Card.Header className="d-flex justify-content-between">
+              <div className="header-title">
+                <h4 className="card-title">Driver Management</h4>
+              </div>
+            </Card.Header>
             
-            
-            {/* table buttons start */}
-            <Navbar expand="lg" className="navbar-light">
-                <div className="header-title col-6">
-                    <h4 className="card-title px-4">Driver Management</h4>
-                </div>
-                <Nav className=" me-auto mb-2 mb-lg-0 col-16">
-                    <Nav variant="pills">
-                        <Button variant="primary m-2 py-1">Add new driver</Button>
-                        <Button variant="primary m-2 py-1">add drivers bulk</Button>
-                    </Nav>
-                </Nav>
-
-            </Navbar>
-            <Navbar expand="lg" className="navbar-light">
-                <Container fluid>
-                <Row>
-                
-                        
-                </Row>
-                    
-                </Container>
-            </Navbar>
-            <Navbar className="navbar-light rounded-1 " expand="lg">
-                <Container fluid>
-                <Dropdown>
-                      show  <Dropdown.Toggle as={Button} className=" bg-primary" size="lg" type="button" id="dropdownMenuButtonSM">
+            {/* /////////////////////////////////////////////////////////////// */}
+        <Navbar className="navbar-light rounded-1 mt-2" expand="lg" >
+            <Container fluid>
+                <Navbar.Brand className="navbar-brand" to="#">
+                     <Dropdown>
+                         show  <Dropdown.Toggle as={Button} className=" bg-primary" size="lg" type="button" id="dropdownMenuButtonSM">
                         10
                         </Dropdown.Toggle> entries
                             <Dropdown.Menu aria-labelledby="dropdownMenuButtonSM">
@@ -175,13 +86,29 @@ export default function PreventiveMaintenance() {
                                 <Dropdown.Item href="#">100</Dropdown.Item>
                             </Dropdown.Menu>
                     </Dropdown> 
-                    <form className="d-flex col-6">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-primary" type="submit">Search</button>
-                </form>
-                </Container>
-            </Navbar>
-            
+                </Navbar.Brand>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbar-2" aria-controls="navbar-2" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+               
+                    <Navbar.Collapse className="collapse" id="navbar-2">
+                        <Nav className=" ms-auto mb-2 mb-lg-0">
+                            <Nav.Item>
+                                <Button variant="primary m-2 py-1">Add New Driver</Button>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Button variant="primary m-2 py-1">Add Drivers bulk</Button>
+                            </Nav.Item>
+                            
+                        </Nav>
+                    </Navbar.Collapse>
+            </Container>
+        </Navbar>
+            {/* /////////////////////////////////////////////////////////////// */}
+
             {/* table buttons end */}
             <Card.Body>
               <div className="table-responsive">
@@ -192,34 +119,28 @@ export default function PreventiveMaintenance() {
                 >
                   <thead>
                     <tr>
-                      <th className="text-center">check</th>
-                      <th className="text-center">Display Name</th>
-                      <th className="text-center">Plate Number</th>
-                      <th className="text-center">Maintenance Type</th>
-                      <th className="text-center">Period Type</th>
-                      <th className="text-center">Start Value</th>
-                      <th className="text-center">Next Value</th>
-                      <th className="text-center">Recurring</th>
-                      <th className="text-center">Notify Period</th>
-                      <th className="text-center">Notify When Value</th>
-                      <th className="text-center">Overdue</th>
+                      <th className="text-center">Full Name</th>
+                      <th className="text-center">Phone number</th>
+                      <th className="text-center">Email</th>
+                      <th className="text-center">Department</th>
+                      <th className="text-center">Licence number</th>
+                      <th className="text-center">Licence expiration date</th>
+                      <th className="text-center">RFID</th>
+                      <th className="text-center">WASL integration</th>
                       <th className="text-center">action</th>
                     </tr>
                   </thead>
                   <tbody>
                     {Data_table.map((item) => (
                       <tr key={item.name}>
-                        <td className="text-center">{item.check}</td>
                         <td className="text-center">{item.name}</td>
-                        <td className="text-center">{item.plateNumber}</td>
-                        <td className="text-center">{item.mainType}</td>
-                        <td className="text-center">{item.periodTyoe}</td>
-                        <td className="text-center">{item.startValue}</td>
-                        <td className="text-center">{item.nextValue}</td>
-                        <td className="text-center">{item.recurring}</td>
-                        <td className="text-center">{item.notifyPeriod}</td>
-                        <td className="text-center">{item.notifyWhenValue}</td>
-                        <td className="text-center">{item.overdue}</td>
+                        <td className="text-center">{item.phoneNumber}</td>
+                        <td className="text-center">{item.email}</td>
+                        <td className="text-center">{item.department}</td>
+                        <td className="text-center">{item.licenceNumber}</td>
+                        <td className="text-center">{item.licenceEXP}</td>
+                        <td className="text-center">{item.rfid}</td>
+                        <td className="text-center">{item.waslIntgeration}</td>
                         <td className="text-center">{item.action}</td>
                       </tr>
                     ))}
@@ -229,9 +150,8 @@ export default function PreventiveMaintenance() {
             </Card.Body>
           </Card>
         </Col>
-        <div className="col-12 mb-5">
-        Showing 1 to 10 of 10 entries
-        </div>
+        <div className="col-12 mb-5">Showing 1 to 10 of 10 entries</div>
       </Row>
     </div>
-  )}
+  );
+}
