220cc6f922fc972d70c9a981855c74cd31aa879a
Abdo2711-coder <a60960764@gmail.com>
2021-10-11 17:30:43 +0200

    try fix

--- pages/Reports.js
+++ pages/Reports.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Row, Col, Card } from "react-bootstrap";
+import { useTranslation } from "next-i18next";
 
 export default function Reports() {
+    const { t } = useTranslation("common");
+
     return (
         <>
             <Row>
                 <Col sm="12">
                     <Card>
-                        <Card.Body>body</Card.Body>
+                        <Card.Body>Reports {t("test")}</Card.Body>
                     </Card>
                 </Col>
             </Row>
