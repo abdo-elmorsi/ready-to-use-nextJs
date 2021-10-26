c04496cb52f3f63bbb0f665ff4a68959ee0a680a
Abdo2711-coder <a60960764@gmail.com>
2021-10-26 09:55:09 +0200

    cc

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styles from '../styles/PageNotFound.module.scss'
+
+const PageNotFound = () => {
+    return (
+        <div className="d-flex justify-content-center align-items-center fs-5 text-danger">
+            <div>404</div>
+        </div>
+    )
+}
+
+export default PageNotFound
